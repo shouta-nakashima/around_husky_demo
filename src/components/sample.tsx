3041a3356a9864605064ff28bf5208523901cfe6
shota nakashima <fantajista5.6nakaji.s@gmail.com>
2021-10-07 08:52:39 +0900

    add to src in pages styles

---
+++ src/components/sample.tsx
@@ -0,0 +1,12 @@
+import {VFC} from 'react';
+
+const sample:VFC = () => {
+  return (
+    <div>
+      <img src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+      <a href="/">test</a>
+    </div>
+  );
+};
+
+export default sample;
